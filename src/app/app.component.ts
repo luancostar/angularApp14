8e4418f9ec7769b808f81359893fbd8718fe5725
Luan <luancostar@gmail.com>
2024-03-15 18:51:10 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfrenderfComponent } from './components/ifrenderf/ifrenderf.component';
+import { EventsComponent } from './components/events/events.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent, DirectivesComponent, IfrenderfComponent],
+  imports: [RouterOutlet, FirstComponentComponent, DirectivesComponent, IfrenderfComponent, EventsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
