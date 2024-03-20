fccdea53ac303cb201d164fda7df1ece7b67c2b5
Luan <luancostar@gmail.com>
2024-03-20 20:35:29 -0300

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+
+ {path: '', component: FirstComponentComponent},
+ {path: 'list', component: ListRenderComponent},
+
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+export class AppRoutingModule { }
